fcc999bd74a4d9b3afeefa406ace4a865558c34e
blacknaml <blacknaml.laits@gmail.com>
2016-06-29 07:06:57 +0700

    Class Adapter untuk handler ListView

--- app/src/main/java/com/masdika/livil2/listviewl2/MainActivity.java
+++ app/src/main/java/com/masdika/livil2/listviewl2/MainActivity.java
@@ -2,6 +2,11 @@ package com.masdika.livil2.listviewl2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ListView listView = (ListView) findViewById(R.id.listview);
+        String[] values = new String[] {
+                "Android", "Iphone", "Windows Mobile", "Blackberry", "WebOS", "Ubuntu", "Windows7", "Mac OS X",
+                "Linux", "OS/2", "Android", "Iphone", "Windows Mobile", "Blackberry", "WebOS", "Ubuntu", "Windows7", "Mac OS X",
+                "Linux", "OS/2"
+        };
+
+        final ArrayList<String> list = new ArrayLIst<String>();
+        for (int i = 0; i < values.length; i++){
+            list.add(values[i]);
+        }
+
+        final StableArrayAdapter adapter = new StableArrayAdapter (this, android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                final String item = (String) parent.getItemAtPosition(position);
+                view.animate().setDuration(2000).alpha(0).withEndAction(new Runnable() {
+                    @Override
+                    public void run() {
+                        list.remove(item);
+                        adapter.notifyDataSetChanged();
+                        view.setAlpha(1);
+                    }
+                })
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/masdika/livil2/listviewl2/StableArrayAdapter.java
@@ -0,0 +1,15 @@
+package com.masdika.livil2.listviewl2;
+
+import android.widget.ArrayAdapter;
+
+import java.util.HashMap;
+
+/**
+ * Created by blacknaml on 29/06/16.
+ */
+public class StableArrayAdapter extends ArrayAdapter<String> {
+
+    HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+    
+}
